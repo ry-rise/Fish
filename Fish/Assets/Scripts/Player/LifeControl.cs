48b03a2436ce0b6af336b842bb4278010ba7cadf
佑亮 合屋 <ryuguren@outlook.jp>
2019-05-14 19:01:23 +0900

    とりあえずPUSH

---
+++ Fish/Assets/Scripts/Player/LifeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeControl : MonoBehaviour
+{
+    [SerializeField]
+    private int maxLife;
+    private int life;
+    public int Life { get { return life; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameReset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameReset()
+    {
+        life = maxLife;
+        //UIバーを初期化
+    }
+}
