e2989f352766ed15a602d16fc96152a346d4094c
ry-rise <ry-rise@example.com>
2019-05-20 23:05:56 +0900

    体力を表示

--- Fish/Assets/Scripts/Player/LifeControl.cs
+++ Fish/Assets/Scripts/Player/LifeControl.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeControl : MonoBehaviour
 {
-    [SerializeField]
-    private float maxLife = 1;
+    [SerializeField] private Text lifeText;
+    [SerializeField] private float maxLife = 1;
     private float life;
     public float Life { get { return life; } }
 
-    // Start is called before the first frame update
     void Start()
     {
         GameReset();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        lifeText.text = $"HP:{Life}";
     }
 
     public void GameReset()
