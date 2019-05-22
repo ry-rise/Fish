46ecac73e3909434648cdaf6d22e83899ce2f438
佑亮 合屋 <ryuguren@outlook.jp>
2019-05-22 12:48:36 +0900

    敵の魚追加

--- Fish/Assets/Scripts/Enemies/BaseEnemyAI.cs
+++ Fish/Assets/Scripts/Enemies/BaseEnemyAI.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseEnemyAI : MonoBehaviour
+abstract public class BaseEnemyAI : MonoBehaviour
 {
+    [SerializeField]
+    protected BaseFish data = null;
+
+    protected Rigidbody2D rb;
+
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        Move();
     }
+
+    abstract protected void Move();
 }
--- Fish/Assets/Scripts/Enemies/BaseFish.cs
+++ Fish/Assets/Scripts/Enemies/BaseFish.cs
@@ -7,20 +7,16 @@ public class BaseFish : ScriptableObject
 {
     //魚の名前
     [SerializeField]
-    private string fishName;
+    private string fishName = "A";
     public string FishName { get { return fishName; } }
     //魚のレベル
     [SerializeField]
-    private int level;
+    private int level = 1;
     public int Level { get { return level; } }
     //速度
     [SerializeField]
-    private float speed;
+    private float speed = 1;
     public float Speed { get { return speed; } }
-    //タイプ
-    [SerializeField]
-    private int type;
-    public int Type { get { return type; } }
     //ツール化
     [MenuItem("Tool/Create Fish")]
     static void CreateAsset()
