354b57c20744d17930b5db577fa8aa9975cbf462
obiya <jupiter-h86@outlook.jp>
2019-05-27 14:00:16 +0900

    タイトル作成開始

---
+++ Fish/Assets/Scripts/OpButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OpButton : MonoBehaviour
+{
+    public void MainGameStart()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
