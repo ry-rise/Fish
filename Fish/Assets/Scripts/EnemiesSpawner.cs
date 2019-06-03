8adacb62091d173344985ed6adc3da2fd0e68d23
ry-rise <ry-rise@example.com>
2019-06-03 14:32:24 +0900

    敵のPrefabを複数作成

--- Fish/Assets/Scripts/EnemiesSpawner.cs
+++ Fish/Assets/Scripts/EnemiesSpawner.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class EnemiesSpawner : MonoBehaviour
 {
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject[] enemies=null;
+    private float spawnTime;
 
     void Update()
     {
-        
+        spawnTime += Time.deltaTime;
+        EnemySpawn();
+    }
+
+    private void EnemySpawn()
+    {
+        if (spawnTime >= 1.0f)
+        {
+            if (enemies != null)
+            {
+                Instantiate(enemies[Random.Range(0, enemies.Length)], transform);
+                spawnTime = 0;
+            }
+        }
     }
 }
